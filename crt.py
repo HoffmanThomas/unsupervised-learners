fcfa4e573ffe227b2d6a3af345c5c0d91b97aed3
wmaloney <maloney_william@bah.com>
2017-06-15 11:17:25 -0400

    cleaned up crt.py and renamed it to knn.py

--- crt.py
+++ crt.py
@@ -8,43 +8,40 @@ from sklearn import cross_validation
 from sklearn import preprocessing
 from sklearn import ensemble
 
+#set a random state for the classifier to use when it partitions the data into training and testing 
 random_state = np.random.RandomState(0) 
+
+#pull in the CSV into the data variable
 data = np.genfromtxt('CRT.csv', delimiter=',', names = True, usecols = (0,1,2,3,4,5,6,7,10)) #this method of import creates a flexible type which does not allow `mean.()` to be called
 
+#set the y data to be the StrengthBin 
 y_data = np.array([data['StrengthBin']]).transpose()
-#y_data = np.array([data['StrengthBin']]).transpose()
-print("\nY DATA \n", y_data)
+
 
 X = np.array([data[x] for x in data.dtype.names])
 scaledData = preprocessing.scale(X) #scale the data
 mean = scaledData.mean(axis = 0) #mean of scaled data
 stdev = scaledData.std(axis = 0) #Std dev of scaled data
 
+#set our data variable to the scaled data version we just created above
 data = scaledData
 
+#set the x data to be the columns other then the y data column 
 x_data = np.array([data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7]]).transpose()
-#x_data = np.array([data['Cement'], data['Slag'], data['FlyAsh'], data['Water'], data['SPlast'], data['CAgg'], data['FAgg'], data['Age']]).transpose()
-print("\nX DATA \n", x_data)
 
 
-
-
-# X_train, X_test, y_train, y_test = cross_validation.train_test_split(x_data, y_data, test_size=.1, random_state=5)
-# knnClassifier = ensemble.RandomForestClassifier(n_estimators=11,random_state=0).fit(X_train, y_train.ravel())
-# #print(knnClassifier.predict(X_test))
-# print(knnClassifier.score(X_test,y_test))
+#block of code below runs through all test vs. train ratios and k values from 1-max_k to determine the variables to maximize score
 
 max1 = 0
 ts = .05
 k = 10
 opt_k =0
 opt_ts = 0
+max_k = 20
 while ts < .90:
-	while k < 20:
+	while k < max_k:
 		X_train, X_test, y_train, y_test = cross_validation.train_test_split(x_data, y_data, test_size=ts, random_state=random_state)
 		knnClassifier = ensemble.RandomForestClassifier(n_estimators=k,random_state=0).fit(X_train, y_train.ravel())
-		#print('\nNum Nearest Neighbors:',k, 'Test Size Split ', ts)
-		#print(knnClassifier.predict(X_test))
 		score = knnClassifier.score(X_test,y_test)
 		if score > max1:
 			max1 = score
@@ -55,7 +52,7 @@ while ts < .90:
 	k=10	
 	ts+=.05
 
-
+#print out the max score we recieved and the variables used to get it 
 print ('Max Score: ',max1)
 print ('Best k for that score: ',opt_k)
 print ('Best test size for that score: ',opt_ts)
