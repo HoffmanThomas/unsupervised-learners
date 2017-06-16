6b9060dfde18fa3cc07dbb4b2b83f56e4cb330b7
Jessica Li <Li_Jessica@bah.com>
2017-06-16 14:28:26 -0400

    With partitioning!

--- regression/BeerNN_in_R.R
+++ regression/BeerNN_in_R.R
@@ -13,28 +13,30 @@ beer1<-beer
 k<-ncol(beer)  #number of predictors
 beer1[2:k]<-sapply(beer1[2:k], function(x) (x-mean(x))/sd(x)) #standardize predictors
 
+##75% of the sample size
+smp_size<-floor(0.75*nrow(beer1))
+#Set seed to make partition reproducible
+set.seed(123)
+train_ind<-sample(seq_len(nrow(beer1)), size=smp_size)
+train<-beer1[train_ind,]
+test<-beer1[-train_ind,]
+
+
 #Fit Neural Network
 library(nnet)
 library(lattice)
 library(ggplot2)
 library(caret)
-nn1<-nnet(beer1$V1~., data=beer1, linout=F, skip=F, size=10, decay=0.01, maxit=1000, trace=F)
-
-#output the class probabilities
-phat<-predict(nn1,type="raw")
-apply(phat,1,sum)
+nn1<-nnet(train$V1~., data=train, linout=F, skip=F, size=10, decay=0.01, maxit=1000, trace=F)
 
 #Predict class
-yhat<-predict(nn1, type="class")
+yhat<-predict(nn1, newdata=test, type="class")
 yhat
 
 #Output misclassification rate
-y<-beer1$V1
+y<-test$V1
 sum(y !=yhat)/length(y)
 
+#Output Prediction rate
+1-sum(y !=yhat)/length(y)
 
-#Cross validation
-Nrep<-20 #number of replications of CV
-K<-10 #K-Fold
-n=nrow(beer1)
-y<-beer1$V1
