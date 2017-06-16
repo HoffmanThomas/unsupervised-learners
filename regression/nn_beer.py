3df754b5fc9d99fe0d062241a55be96d49167171
wmaloney <maloney_william@bah.com>
2017-06-16 11:02:03 -0400

    Clean version

--- regression/nn_beer.py
+++ regression/nn_beer.py
@@ -25,14 +25,16 @@ X = stats.zscore(X, axis=0) # normalize the data by feature
 #Sets m as the number of rows in X (X.shape[1] would be the number of columns)
 num_rows = X.shape[0] # number of data points
 num_cols = X.shape[1]
-
+print (num_rows)
 #Pybrain cannot take in a normal array as data to the classifiers so we need to convert our data array to something it can use and then populate it
 data = ClassificationDataSet(num_cols)
 for i in range(0,num_rows):
     data.addSample(X[i,:],y[i])
 
+
 #Converts the data to a vector 
 data._convertToOneOfMany()
+
 
 #Split the data into a set proportion between training and testing 
 tstdata, trndata = data.splitWithProportion(0.25)
@@ -40,11 +42,8 @@ tstdata, trndata = data.splitWithProportion(0.25)
 #create our neural net 
 net = buildNetwork(data.indim,20,data.outdim, bias=True, outputbias=True, outclass=SigmoidLayer)
 
-
-
 #Create the training data
 trainer = BackpropTrainer(net, dataset=trndata, learningrate=5, momentum=0.01, weightdecay=0, verbose=True)
-# p = net.activateOnDataset( data )
-# print (p)
+
 
 trainer.trainUntilConvergence()
