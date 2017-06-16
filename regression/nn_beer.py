6bf425e293d9489dfe861decb531b70ab39232a6
wmaloney <maloney_william@bah.com>
2017-06-16 10:49:19 -0400

    Fixed issues and cleaned up original code

--- regression/nn_beer.py
+++ regression/nn_beer.py
@@ -26,28 +26,16 @@ X = stats.zscore(X, axis=0) # normalize the data by feature
 num_rows = X.shape[0] # number of data points
 num_cols = X.shape[1]
 
-#Pybrain cannot take in a normal array as data to the classifiers so we need to convert our data array to something it can use and then populate it 
-
+#Pybrain cannot take in a normal array as data to the classifiers so we need to convert our data array to something it can use and then populate it
 data = ClassificationDataSet(num_cols)
 for i in range(0,num_rows):
     data.addSample(X[i,:],y[i])
 
+#Converts the data to a vector 
+data._convertToOneOfMany()
 
 #Split the data into a set proportion between training and testing 
-tstdata_temp, trndata_temp = data.splitWithProportion(0.25)
-
-#Since pybrain is broken we need this workaround to convert the trndata array to the correct dimension
-tstdata = ClassificationDataSet(13, 1, nb_classes=3)
-for n in range(0, tstdata_temp.getLength()):
-    tstdata.addSample( tstdata_temp.getSample(n)[0], tstdata_temp.getSample(n)[1] )
-
-trndata = ClassificationDataSet(13, 1, nb_classes=3)
-for n in range(0, trndata_temp.getLength()):
-    trndata.addSample( trndata_temp.getSample(n)[0], trndata_temp.getSample(n)[1] )
-
-#Converts the data to a vector
-data._convertToOneOfMany()
-trndata._convertToOneOfMany()
+tstdata, trndata = data.splitWithProportion(0.25)
 
 #create our neural net 
 net = buildNetwork(data.indim,20,data.outdim, bias=True, outputbias=True, outclass=SigmoidLayer)
@@ -59,4 +47,4 @@ trainer = BackpropTrainer(net, dataset=trndata, learningrate=5, momentum=0.01, w
 # p = net.activateOnDataset( data )
 # print (p)
 
-#trainer.trainUntilConvergence()
+trainer.trainUntilConvergence()
