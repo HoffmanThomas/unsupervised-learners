76d138f47e84eeca9ae2b7d6e6fe15b672d18f95
Thomas Hoffman <hoffman_thomas@bah.com>
2017-06-19 08:21:02 -0400

    r in py edit

---
+++ py_r/BeerNN_in_R.R
@@ -0,0 +1,41 @@
+#Beer data NN in R
+#BOOZED allen hamilton lol
+
+
+#Import data
+beer <- read.csv("beerdataEdit.csv",header=FALSE)
+#We want to fit a model that esitmates the first column
+
+#Scale data
+beer1<-beer
+k<-ncol(beer)  #number of predictors
+beer1[2:k]<-sapply(beer1[2:k], function(x) (x-mean(x))/sd(x)) #standardize predictors
+
+##75% of the sample size
+smp_size<-floor(0.75*nrow(beer1))
+#Set seed to make partition reproducible
+set.seed(123)
+train_ind<-sample(seq_len(nrow(beer1)), size=smp_size)
+train<-beer1[train_ind,]
+test<-beer1[-train_ind,]
+
+#Fit Neural Network
+require(nnet);
+require(lattice)
+require(ggplot2)
+require(caret)
+
+nn1<-nnet(train$V1~., data=train, linout=F, skip=F, size=10, decay=0.01, maxit=1000, trace=F)
+
+#Predict class
+yhat<-predict(nn1, newdata=test, type="class")
+
+#Output misclassification rate
+y<-test$V1
+
+cat(sum(y !=yhat)/length(y))
+
+#Output Prediction rate
+cat(1-sum(y !=yhat)/length(y))
+
+cat(yhat)
